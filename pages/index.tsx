af7c80fef6cbd6eb8aa687b4d90b5242a97fa0c2
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-12-16 21:47:02 +0530

    socket draw utils and setup, socket listen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,86 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useRef, useState } from "react";
+import { useDraw } from "@/common/hooks/useDraw";
+import { socket } from "@/common/lib/socket";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D>();
+
+  const [size, setSize] = useState({ width: 0, height: 0 });
+  const [options, setOptions] = useState<CtxOptions>({
+    lineColor: "#000",
+    lineWidth: 5,
+  });
+
+  const { drawing, handleDraw, handleEndDrawing, handleStartDrawing } = useDraw(
+    options,
+    ctxRef.current
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) ctxRef.current = ctx;
+    }
+  }, [options.lineColor, options.lineWidth]);
+
+  const drawFromSocket = (
+    socketMoves: [number, number][],
+    socketOptions: CtxOptions
+  ) => {
+    const tempCtx = ctxRef.current;
+    if (tempCtx) {
+      (tempCtx.lineWidth = socketOptions.lineWidth),
+        (tempCtx.strokeStyle = socketOptions.lineColor);
+
+      tempCtx.beginPath();
+      socketMoves.forEach(([x, y]) => {
+        tempCtx.lineTo(x, y);
+        tempCtx.stroke();
+      });
+      tempCtx.closePath();
+    }
+  };
+
+  // Start listening for the events that are emitted from server.
+  useEffect(() => {
+    let movesToDrawLater: [number, number][] = [];
+    let optionsToUseLater: CtxOptions = {
+      lineColor: "",
+      lineWidth: 0,
+    };
+    socket.on("socket_draw", (movesToDraw, socketOptions) => {
+      if (ctxRef.current && !drawing) {
+        drawFromSocket(movesToDraw, socketOptions);
+      } else {
+        movesToDrawLater = movesToDraw;
+        optionsToUseLater = socketOptions;
+      }
+    });
+    return () => {
+      socket.off("socket_draw");
+      if (movesToDrawLater.length) {
+        drawFromSocket(movesToDrawLater, optionsToUseLater);
+      }
+    };
+  }, [drawing]);
+
   return (
     <div>
       <h1 className="bg-red-500">Template</h1>
