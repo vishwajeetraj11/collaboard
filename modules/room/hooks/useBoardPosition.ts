360ae505e3ac2cf8394c431fb7fafd9f21332a34
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-12-18 22:53:14 +0530

    rename

---
+++ modules/room/hooks/useBoardPosition.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react"
+import { RoomContext } from "../context/room.context";
+
+export const useBoardPosition = () => {
+    const { x, y } = useContext(RoomContext);
+    return { x, y }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import { useEffect, useRef, useState } from "react";
 import { useDraw } from "@/common/hooks/useDraw";
 import { socket } from "@/common/lib/socket";
-import Canvas from "@/modules/canvas/components/Canvas";
+import Canvas from "@/modules/room/components/Canvas";
 
 const inter = Inter({ subsets: ["latin"] });
 
